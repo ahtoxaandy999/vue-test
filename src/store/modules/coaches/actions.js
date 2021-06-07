6b42a3c99634dcd5279365a83f9c75758ba55fd4
tolkanec96 <tolkanec96@gmail.com>
2021-06-07 17:15:16 +0300

    Upd

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,7 +10,7 @@ export default {
     };
 
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches/${userId}.json`,
+      `https://test1-9e949-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
@@ -34,9 +34,10 @@ export default {
     }
 
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches.json`
+      `https://test1-9e949-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
     );
     const responseData = await response.json();
+    console.log(responseData);
 
     if (!response.ok) {
       const error = new Error(responseData.message || 'Failed to fetch!');
